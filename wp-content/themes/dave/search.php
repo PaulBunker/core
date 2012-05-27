5de459a34db47c3fc140f0cd0f0795d8a2db5f3e
Tom Schwarz <TPSchwarz@gmail.com>
2012-05-27 16:55:46 +0100

    moved sidebar to the left

--- wp-content/themes/dave/search.php
+++ wp-content/themes/dave/search.php
@@ -1,6 +1,13 @@
 <?php get_header(); ?>
   <?php roots_content_before(); ?>
     <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
+    <?php roots_sidebar_before(); ?>
+      <aside id="sidebar" class="<?php echo SIDEBAR_CLASSES; ?>" role="complementary">
+      <?php roots_sidebar_inside_before(); ?>
+        <?php get_sidebar(); ?>
+      <?php roots_sidebar_inside_after(); ?>
+      </aside><!-- /#sidebar -->
+    <?php roots_sidebar_after(); ?>
     <?php roots_main_before(); ?>
       <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
         <div class="page-header">
@@ -11,13 +18,6 @@
         <?php roots_loop_after(); ?>
       </div><!-- /#main -->
     <?php roots_main_after(); ?>
-    <?php roots_sidebar_before(); ?>
-      <aside id="sidebar" class="<?php echo SIDEBAR_CLASSES; ?>" role="complementary">
-      <?php roots_sidebar_inside_before(); ?>
-        <?php get_sidebar(); ?>
-      <?php roots_sidebar_inside_after(); ?>
-      </aside><!-- /#sidebar -->
-    <?php roots_sidebar_after(); ?>
     </div><!-- /#content -->
   <?php roots_content_after(); ?>
 <?php get_footer(); ?>
