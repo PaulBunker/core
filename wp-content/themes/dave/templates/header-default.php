668811732f076513e4bebdd557323b7b2093735f
Paul Bunker <paulmarkbunker@gmail.com>
2012-05-27 17:23:07 +0100

    changed css for logo

--- wp-content/themes/dave/templates/header-default.php
+++ wp-content/themes/dave/templates/header-default.php
@@ -1,8 +1,9 @@
 <header id="banner" role="banner">
   <?php roots_header_inside(); ?>
   <div class="<?php echo WRAP_CLASSES; ?>">
-    <a class="brand" href="<?php echo home_url(); ?>/">
-      <?php bloginfo('name'); ?>
+    <a class="brand <?php echo HEADER_CLASSES; ?>" href="<?php echo home_url(); ?>/">
+      <span class='dave'>Dave</span><br />
+      <span class='dalziel'>Dalziel</span>
     </a>
   </div>
 </header>
