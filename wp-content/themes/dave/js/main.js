e7a7350476240cc929d796362b1b73dd9681c94a
Paul Bunker <paulmarkbunker@gmail.com>
2012-06-02 15:07:00 +0100

    added custom build of jquery ui with only accordian widget. implemented functionality on the page

--- wp-content/themes/dave/js/main.js
+++ wp-content/themes/dave/js/main.js
@@ -1,7 +1,9 @@
-/* Author:
-
-*/
-
-
-
-
+//js for dave
+$(document).ready(function() {
+//add accordian behaviour to the nav menu
+	$('.menu').accordion({
+		event: "mouseover"
+	}); 
+//end dom.ready	 
+});
+//EOF
