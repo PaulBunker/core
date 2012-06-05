f144a03258892941a26175ded88e7ce947934ae0
Paul Bunker <paulmarkbunker@gmail.com>
2012-06-05 14:11:11 +0100

    added sticky nav functionality

--- wp-content/themes/dave/js/main.js
+++ wp-content/themes/dave/js/main.js
@@ -1,10 +1,29 @@
-//js for dave
-$(document).ready(function() {
+// js for dave
+$(document).ready(function() { // dom ready
+
 //add accordian behaviour to the nav menu
+
 	$('.menu').accordion({
 		event: "mouseover",
 		navigation: true
 	}); 
-//end dom.ready	 
-});
-//EOF
+	
+//sticky nav
+
+	var stickyTop = $('.daveWell').offset().top -20; // returns number
+	
+	$(window).scroll(function(){ // scroll event
+	
+    	var windowTop = $(window).scrollTop(); // returns number
+		
+		if (stickyTop < windowTop) {
+      		$('.daveWell').css({ position: 'fixed', top: 0 });
+    	} else {
+      		$('.daveWell').css('position','static');
+    	}
+	
+	}); // end scroll event
+
+	 
+}); // end dom.ready
+// EOF
