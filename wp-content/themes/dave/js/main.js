09a3ecd9c50ffd265d783baa155c4a5c037a068c
Paul Bunker <paulmarkbunker@gmail.com>
2012-06-27 16:25:02 +0100

    fix for menu sizing issue. wrong branch though! sorry

--- wp-content/themes/dave/js/main.js
+++ wp-content/themes/dave/js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function() { // dom ready
     	var windowTop = $(window).scrollTop(); // returns number
 		
 		if (stickyTop < windowTop) {
-      		$('.daveWell').css({ position: 'fixed', top: 0 });
+      		$('.daveWell').css({ position: 'fixed', top: 0,  });
     	} else {
       		$('.daveWell').css('position','static');
     	}
