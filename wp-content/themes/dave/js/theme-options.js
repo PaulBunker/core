10797d114ab994ec71599ae8e0e26b56fbeb838a
Paul Bunker <paulmarkbunker@gmail.com>
2012-07-12 22:21:59 +0100

    added missing file

---
+++ wp-content/themes/dave/js/theme-options.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+        
+    $('#colorpicker').hide();
+	$('#colorpicker').farbtastic('#color');
+
+    $('#color').click(function() {
+        $('#colorpicker').fadeIn();
+    });
+
+    $(document).mousedown(function() {
+        $('#colorpicker').each(function() {
+            var display = $(this).css('display');
+            if ( display == 'block' )
+                $(this).fadeOut();
+        });
+    });
+});
